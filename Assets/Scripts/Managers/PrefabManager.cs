b0f84c509c2e2f6ac232fe3d8c0ad7334eade741
MehmetScgn <mehmetascgn@gmail.com>
2023-02-05 17:24:02 +0200

    NS-004 [Species] Create States, Add food, Change between states
    * Add food prototype

--- Assets/Scripts/Managers/PrefabManager.cs
+++ Assets/Scripts/Managers/PrefabManager.cs
@@ -5,10 +5,16 @@ namespace Managers
     public class PrefabManager : MonoBehaviour
     {
         [SerializeField] private GameObject speciesPrefab;
+        [SerializeField] private GameObject foodPrefab;
 
         public GameObject GetSpeciesPrefab()
         {
             return speciesPrefab;
+        }
+
+        public GameObject GetFoodPrefab()
+        {
+            return foodPrefab;
         }
     }
 }
